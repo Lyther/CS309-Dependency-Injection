48b41b0bedf8ca66a7af5203f3a131af3c09d3c6
Enderaoe <enderaoelyther@gmail.com>
2019-12-11 11:13:01 +0800

    finish

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ CS309 Assignment 4, learn how to use dependency injection.
 
 ## Current progress:
 
+Added some comment to the code.
+
 Added the bonus test case to the unit test class.
 
-Passed 12 tests of 18 standard test cases.
+Passed all the 18 test cases already.
--- src/main/java/cn/edu/sustech/ContainerImpl.java
+++ src/main/java/cn/edu/sustech/ContainerImpl.java
@@ -1,6 +1,8 @@
 package cn.edu.sustech;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -38,19 +40,43 @@ public class ContainerImpl implements Container {
     public <T> T resolve(Class<T> serviceType) {
         if (serviceType == null) throw new IllegalArgumentException();
         if (!container.contains(serviceType)) throw new ServiceNotFoundException();
+
         try {
-            Constructor[] constructors = serviceType.getConstructors();
+            Field[] fields = serviceType.getDeclaredFields();
+            Constructor[] constructors = containerImpl.containsKey(serviceType) ? containerImpl.get(serviceType).getConstructors() : serviceType.getConstructors();
+
+            // create instance directly from constructor
             Class[] parameterTypes = constructors[0].getParameterTypes();
             List<Object> parameters = new ArrayList<>();
             for (Class parameter : parameterTypes) {
                 if (!container.contains(parameter)) throw new ServiceNotFoundException();
                 for (Class instance : container)
                     if (instance.getTypeName().equals(parameter.getTypeName())) {
-                        parameters.add(resolve(instance));
+                        parameters.add(resolve(instance));  // Recursively resolve instance
                         break;
                     }
             }
-            return (T) constructors[0].newInstance(parameters.toArray());
+
+            // create instance without field values
+            T result = (T) constructors[0].newInstance(parameters.toArray());
+
+            // set field value according to @Inject annotation
+            for (Field field : fields) {
+                Annotation annotation = field.getAnnotation(Inject.class);
+                if (annotation != null) {
+                    if (container.contains(field.getType())) {
+                        for (Class instance : container)
+                            if (instance == field.getType()) {
+                                field.setAccessible(true);
+                                field.set(result, resolve(instance));   // recursively resolve field instance
+                                break;
+                            }
+                    } else
+                        throw new ServiceNotFoundException();
+                }
+            }
+
+            return result;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             return null;
         }
